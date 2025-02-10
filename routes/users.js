105ed4ed011c529dd13516599e0e6c1344f1afa4
amit-eliyahu <amit270399@gmail.com>
2025-02-10 17:54:44 +0200

    Fixed issues in users.js and add.js

--- routes/users.js
+++ routes/users.js
@@ -13,21 +13,12 @@ router.get('/:userid', async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    // חיפוש כל ההוצאות של המשתמש
-    const costs = await Cost.find({ userid: userId });
-
-    // חישוב הסכום הכולל של ההוצאות
-    let totalSum = 0;
-    for (const cost of costs) {
-      totalSum += cost.sum;
-    }
-
     // החזרת הנתונים
     res.status(200).json({
       first_name: user.first_name,
       last_name: user.last_name,
       id: userId,
-      total: totalSum
+      total: user.totalCost // מחזיר את ה-totalCost ששמור אצל המשתמש
     });
 
   } catch (error) {
