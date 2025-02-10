1a9684c1784f6b47defc1098076d495f46a360df
amit-eliyahu <amit270399@gmail.com>
2025-02-10 17:26:02 +0200

    Fixed issues in users.js

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const User = require('../models/user');
 const Cost = require('../models/cost');
 
-// קבלת פרטי משתמש וסכום כל ההוצאות שלו
 router.get('/:userid', async (req, res) => {
   const userId = req.params.userid;
 
   try {
-    // חיפוש המשתמש במסד הנתונים
-    const user = await User.findById(userId);
+    // חיפוש המשתמש לפי userId שהוא מחרוזת (ולא ObjectId)
+    const user = await User.findOne({ userid: userId });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
@@ -17,13 +16,13 @@ router.get('/:userid', async (req, res) => {
     // חיפוש כל ההוצאות של המשתמש
     const costs = await Cost.find({ userid: userId });
 
-    // חישוב הסכום הכולל של ההוצאות ללא reduce
+    // חישוב הסכום הכולל של ההוצאות
     let totalSum = 0;
     for (const cost of costs) {
       totalSum += cost.sum;
     }
 
-    // החזרת נתוני המשתמש והסכום הכולל
+    // החזרת הנתונים
     res.status(200).json({
       first_name: user.first_name,
       last_name: user.last_name,
